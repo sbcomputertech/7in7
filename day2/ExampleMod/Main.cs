59384f3564ad23064e299a68a9d34aa680e50821
sbcomputertech <sbcomputertech.0@gmail.com>
2022-11-22 19:27:52 +0000

    Day 2!!! get ready for big booms

---
+++ day2/ExampleMod/Main.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ExampleMod
+{
+    [BepInPlugin(ModGuid, ModName, ModVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        private const string ModName = "7in7 Day 2";
+        private const string ModAuthor  = "reddust9";
+        private const string ModGuid = "com.reddust9.7in7.day2";
+        private const string ModVersion = "1.0.0";
+        public static ManualLogSource logger;
+        internal void Awake()
+        {
+            // Creating new harmony instance
+            var harmony = new Harmony(ModGuid);
+
+            // Applying patches
+            harmony.PatchAll();
+            Logger.LogInfo($"{ModName} successfully loaded! Made by {ModAuthor}");
+            logger = Logger;
+        }
+
+        [HarmonyPatch(typeof(SelectionHelper), nameof(SelectionHelper.PickRandomWeapon))]
+        public class P_1
+        {
+            public static bool Prefix(List<SpawnableWeapon> weapons, ref SpawnableWeapon __result)
+            {
+                foreach (var wep in weapons)
+                {
+                    if (wep.weaponObject.name is not "BigGrenade") continue;
+                    __result = wep;
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
